d0c7c8ab97b179457c224858c8de9f07fef773fd
Balanceact <makildur@hotmail.com>
2024-06-12 11:39:21 +0200

    Expanded on Vehicle class and sub-classes.

--- GarageInventory/Vehicle.cs
+++ GarageInventory/Vehicle.cs
@@ -9,11 +9,33 @@ namespace GarageInventory
     internal class Vehicle
     {
         private string _licensePlateNumber;
+        private string _color;
+        private int _numberOfWheels;
+        private string _make;
+        private string _model;
+        private int _year;
+        private string _description;
+        private bool _forRent;
 
-        public string LicensePlateNumber
+        public string LicensePlateNumber { get { return _licensePlateNumber; } set { _licensePlateNumber = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+        public int NumberOfWheels { get { return _numberOfWheels; } set { _numberOfWheels = value; } }
+        public string Make { get { return _make; } set { _make = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public int Year { get { return _year; } set { _year = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public bool ForRent { get { return _forRent; } set { _forRent = value; } }
+
+        public Vehicle(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent)
         {
-            get { return _licensePlateNumber; }
-            set { _licensePlateNumber = value; }
+            _licensePlateNumber = licensePlateNumber;
+            _color = color;
+            _numberOfWheels = numberOfWheels;
+            _make = make;
+            _model = model;
+            _year = year;
+            _description = description;
+            _forRent = forRent;
         }
 
     }
@@ -22,30 +44,64 @@ namespace GarageInventory
         private int _wingspan;
 
         public int Wingspan { get { return _wingspan; } set { _wingspan = value; } }
+
+        public Airplane(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent, int wingspan) 
+            : base(licensePlateNumber, color, numberOfWheels, make, model, year, description, forRent)
+        {
+            _wingspan = wingspan;
+        }
+
     }
     internal class Boat : Vehicle
     {
         private int _length;
 
         public int Length { get { return _length; } set { _length = value; } }
+
+        public Boat(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent, int length) 
+            : base(licensePlateNumber, color, numberOfWheels, make, model, year, description, forRent)
+        {
+            _length = length;
+        }
+
     }
     internal class Bus : Vehicle
     {
         private int _numberOfSeats;
 
         public int NumberOfSeats { get { return _numberOfSeats; } set { _numberOfSeats = value; } }
+
+        public Bus(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent, int numberOfSeats) 
+            : base(licensePlateNumber, color, numberOfWheels, make, model, year, description, forRent)
+        {
+            _numberOfSeats = numberOfSeats;
+        }
+
     }
     internal class Car : Vehicle
     {
-        private int _fuelType;
+        private string _fuelType;
+        
+        public string FuelType { get { return _fuelType; } set { _fuelType = value; } }
 
-        public int FuelType { get { return _fuelType; } set { _fuelType = value; } }
+        public Car(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent, string fuelType)
+            : base(licensePlateNumber, color, numberOfWheels, make, model, year, description, forRent)
+        {
+            _fuelType = fuelType;
+        }
+
     }
     internal class Motorcycle : Vehicle
     {
-        private int _cylinderVolume;
+        private int _cylinderVolumeInCC;
 
-        public int CylinderVolume { get { return _cylinderVolume; } set { _cylinderVolume = value; } }
+        public int CylinderVolumeInCC { get { return _cylinderVolumeInCC; } set { _cylinderVolumeInCC = value; } }
+
+        public Motorcycle(string licensePlateNumber, string color, int numberOfWheels, string make, string model, int year, string description, bool forRent, int cylinderVolumeInCC) 
+            : base(licensePlateNumber, color, numberOfWheels, make, model, year, description, forRent)
+        {
+            _cylinderVolumeInCC = cylinderVolumeInCC;
+        }
 
     }
 }
