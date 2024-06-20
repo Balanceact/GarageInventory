088d7a2beb5799ca19fa00cef76fe7adf668108b
Balanceact <makildur@hotmail.com>
2024-06-20 10:20:39 +0200

    Added GarageTest Class.

---
+++ GarageInventory.Tests/GarageTest.cs
@@ -0,0 +1,63 @@
+using Xunit;
+using Moq;
+
+namespace GarageInventory.Tests
+{
+    public class GarageTest                                                 //ToDo: xUnit and Moq!
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeProperties()                //ToDo: Implement test!
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_OnInvalidCapacity()    //ToDo: Implement test!
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void GetEnumerator_ShouldNeverReturnTheSameObjectTwice()     //ToDo: Implement test!
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void Remove_ShouldRemoveElementFromArray()                   //ToDo: Implement test!
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void AllParkedToString_ShouldReturnListOfString()            //ToDo: Implement test!
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
--- GarageInventory/Program.cs
+++ GarageInventory/Program.cs
@@ -21,16 +21,7 @@ namespace GarageInventory
             host.Services.GetRequiredService<Manager>().Run();
         }
     }
-}/* Functionality:
-        It should be possible to:
-            Search for vehicles based on one or more properties (all possible combinations from the base class Vehicle). For example:
-                All black vehicles with four wheels.
-                All pink motorcycles with 3 wheels.
-                All trucks.
-                All red vehicles.
-        The application should robustly handle input errors, so it does not crash with incorrect input or usage.
-    Unit Testing:
+}/* Unit Testing:
         Tests should be created in a separate test project. We limit ourselves to testing the public methods in the Garage class.
     Extra:
-        It is possible in C# to write to and read from the file system from your application. Find out how to do this to be able to save your garage and load your garage.
         (Writing tests for the entire application is considered an extra task if time permits) */
